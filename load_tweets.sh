99a5147ddabe366f354d3968c79433ba5d80954d
maxplush <maxabeplush@gmail.com>
2025-04-07 10:02:18 -0700

    reverted to update of the

--- load_tweets.sh
+++ load_tweets.sh
@@ -1,34 +1,18 @@
 #!/bin/sh
 
 # list all of the files that will be loaded into the database
+# for the first part of this assignment, we will only load a small test zip file with ~10000 tweets
+# but we will write are code so that we can easily load an arbitrary number of files
 files='
 test-data.zip
 '
 
 echo 'load normalized'
-for file in $files
-do
+for file in $files; do
     # call the load_tweets.py file to load data into pg_normalized
-    echo "Loading $file into pg_normalized..."
-    # python3 load_tweets.py "$file"
 done
 
-# Commented out to avoid breaking test cases with too much data
-: <<'DISABLE_DENORMALIZED'
 echo 'load denormalized'
-for file in $files
-do
-    echo "Extracting and loading $file into pg_denormalized..."
-
-    unzip -o "$file" -d /tmp/load-denorm
-
-    for json_file in /tmp/load-denorm/*.json
-    do
-        echo "Loading $json_file..."
-
-        cat "$json_file" | sed 's/\\u0000//g' | psql "$PG_DENORMALIZED_URL" -c \
-        "COPY tweets (json_data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
-    done
+for file in $files; do
+    # use SQL's COPY command to load data into pg_denormalized
 done
-DISABLE_DENORMALIZED
-
