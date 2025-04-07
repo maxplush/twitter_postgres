3e5c21aacf92cca7ce33c487b73d0dd9a6881493
maxplush <maxabeplush@gmail.com>
2025-04-07 10:54:40 -0700

    updated tests

--- load_tweets.sh
+++ load_tweets.sh
@@ -10,6 +10,7 @@ test-data.zip
 echo 'load normalized'
 for file in $files; do
     # call the load_tweets.py file to load data into pg_normalized
+    python3 load_tweets.py --db=postgresql://postgres:pass@host:1569/database --file="$file"
 done
 
 echo 'load denormalized'
